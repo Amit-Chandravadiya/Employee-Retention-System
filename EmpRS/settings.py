5c6edbc29cc957fa764d2a8e5d8aba721ba5256d
Amit-Chandravadiya <73899544+Amit-Chandravadiya@users.noreply.github.com>
2022-04-03 11:03:32 +0530

    Update settings.py

--- EmpRS/settings.py
+++ EmpRS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=2te$(993+1ny3_k^lhwoefx!+zyv3rx@i+dyujyobjt@(m0ay'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','employee-retention-system.herokuapp.com']
 
